e7584888d38e06f58d55592b2cc514d824cbc49b
cbrownmactc <cbrown@muskegonisd.org>
2026-01-13 11:22:42 -0500

    Fixed newline / input mangling.

--- Engine/PlayerNetwork.cs
+++ Engine/PlayerNetwork.cs
@@ -10,11 +10,13 @@ namespace RPGFramework
         public TelnetConnection? TelnetConnection { get; set; }
         public StreamWriter Writer { get; }
         public StreamReader Reader { get; }
+        public bool NeedsOutputNewline { get { return _needsOutputNewLine; } }
 
+        private bool _needsOutputNewLine;
         public PlayerNetwork(TcpClient client)
         {
             Client = client;
-           
+
             NetworkStream stream = client.GetStream();
 
             Writer = new StreamWriter(stream, new UTF8Encoding(false))
@@ -26,6 +28,7 @@ namespace RPGFramework
             //Writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
             //Reader = new StreamReader(stream, Encoding.UTF8);
             TelnetConnection = new TelnetConnection(stream, Encoding.UTF8);
+            TelnetConnection.OnlineCommitted = () => _needsOutputNewLine = true;
         }
     }
 }
--- Engine/TelnetServer.cs
+++ Engine/TelnetServer.cs
@@ -15,7 +15,7 @@ internal class TelnetServer
 {
     private TcpListener _listener;
     private bool _isRunning;
-    
+
 
     public TelnetServer(int port)
     {
@@ -44,18 +44,18 @@ internal class TelnetServer
         {
             // Create PlayerNetwork object, once logged in we'll attach it to player
             PlayerNetwork pn = new PlayerNetwork(client);
-            
+
 
             pn.Writer.WriteLine("Username: ");
             //string? playerName = pn.Reader.ReadLine();
             string? playerName = await pn.TelnetConnection.ReadLineAsync();
-            
+
             while (string.IsNullOrEmpty(playerName))
             {
                 pn.Writer.WriteLine("Username: ");
                 playerName = pn.TelnetConnection.ReadLine();
             }
-            
+
             GameState.Log(DebugLevel.Debug, $"Player '{playerName}' is connecting...");
             Player player;
 
@@ -63,7 +63,7 @@ internal class TelnetServer
             if (GameState.Instance.Players.ContainsKey(playerName))
             {
                 GameState.Log(DebugLevel.Debug, $"Existing player '{playerName}' found, loading data...");
-                player = GameState.Instance.Players[playerName];                
+                player = GameState.Instance.Players[playerName];
             }
             else
             {
